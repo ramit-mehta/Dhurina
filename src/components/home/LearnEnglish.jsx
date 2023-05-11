fdb59d4bea4c89c01b91d607cd1e33f0a863d0b1
xenosnikos <nxenos@me.com>
2023-05-11 19:03:09 +0530

    learn section responsive

--- src/components/home/LearnEnglish.jsx
+++ src/components/home/LearnEnglish.jsx
@@ -16,90 +16,96 @@ const LearnEnglish = () => {
   };
 
   return (
-    <div className="custom_container container py-5">
+    <div className="custom_container container py-2 py-sm-5">
       <Slider {...settings}>
-        <div className="row d-flex align-items-center learn py-4 py-lg-0">
-          <div className="col-lg-4 position-relative">
-            <img src={BgEllipse} alt="BgEllipse" />
-            <img
-              className="position-absolute janhavi_img start-50 translate-middle-x "
-              src={JanhaviImg}
-              alt="JanhaviImg"
-            />
-          </div>
-          <div className="col-lg-6">
-            <h2 className="ff_inter fw-bold fs_9xl text_gradient mb-0 text-center text-lg-start mt-2 mt-lg-0">
-              Learn English{" "}
-            </h2>
-            <p className="ff_inter fw-semibold fs_6xl color_dark_blue mb-0 text-center text-lg-start">
-              Spoken Courses & Language Courses
-            </p>
-            <p className="ff_inter fs_md color_dark_blue mb-0 text-center text-lg-start">
-              By Wonder Girl Janvavi Panwar
-            </p>
-          </div>
-          <div className="col-lg-2 text-center text-lg-start">
-            <Link to="/register">
-              <button className="fs-6 text-white ff_inter bg_gradient border-0 px-4 py-2 mt-3 mt-lg-0">
-                Register
-              </button>
-            </Link>
-          </div>
-        </div>
-        <div className="row d-flex align-items-center justify-content-center learn py-4 py-lg-0">
-          <div className="col-lg-4 position-relative">
-            <img src={BgEllipse} alt="BgEllipse" />
-            <img
-              className="position-absolute janhavi_img start-50 translate-middle-x "
-              src={JanhaviImg}
-              alt="JanhaviImg"
-            />
-          </div>
-          <div className="col-lg-6">
-            <h2 className="ff_inter fw-bold fs_9xl text_gradient mb-0 text-center text-lg-start mt-2 mt-lg-0">
-              Learn English{" "}
-            </h2>
-            <p className="ff_inter fw-semibold fs_6xl color_dark_blue mb-0 text-center text-lg-start">
-              Spoken Courses & Language Courses
-            </p>
-            <p className="ff_inter fs_md color_dark_blue mb-0 text-center text-lg-start">
-              By Wonder Girl Janvavi Panwar
-            </p>
-          </div>
-          <div className="col-lg-2 text-center text-lg-start">
-            <Link to="/register">
-              <button className="fs-6 text-white ff_inter bg_gradient border-0 px-4 py-2 mt-3 mt-lg-0">
-                Register
-              </button>
-            </Link>
+        <div className="px-sm-3 px-1">
+          <div className="d-flex align-items-center learn py-lg-0">
+            <div className="col-5 col-lg-4 position-relative">
+              <img className="w-100" src={BgEllipse} alt="BgEllipse" />
+              <img
+                className="position-absolute janhavi_img start-50 translate-middle-x "
+                src={JanhaviImg}
+                alt="JanhaviImg"
+              />
+            </div>
+            <div className="col-4 col-lg-6">
+              <h2 className="ff_inter fw-bold fs_9xl text_gradient mb-0 text-center text-lg-start mt-2 mt-lg-0">
+                Learn English{" "}
+              </h2>
+              <p className="ff_inter fw-semibold fs_6xl color_dark_blue mb-0 text-center text-lg-start">
+                Spoken Courses & Language Courses
+              </p>
+              <p className="ff_inter fs_md color_dark_blue mb-0 text-center text-lg-start">
+                By Wonder Girl Janvavi Panwar
+              </p>
+            </div>
+            <div className="col-3 col-lg-2 text-center text-lg-start">
+              <Link to="/register">
+                <button className="fs_xl text-white ff_inter bg_gradient border-0 px-2 py-1 px-sm-4 py-sm-2 ">
+                  Register
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="row d-flex align-items-center learn py-4 py-lg-0">
-          <div className="col-lg-4 position-relative">
-            <img src={BgEllipse} alt="BgEllipse" />
-            <img
-              className="position-absolute janhavi_img start-50 translate-middle-x "
-              src={JanhaviImg}
-              alt="JanhaviImg"
-            />
+        <div className="px-sm-3 px-1">
+          <div className="d-flex align-items-center learn py-lg-0">
+            <div className="col-5 col-lg-4 position-relative">
+              <img className="w-100" src={BgEllipse} alt="BgEllipse" />
+              <img
+                className="position-absolute janhavi_img start-50 translate-middle-x "
+                src={JanhaviImg}
+                alt="JanhaviImg"
+              />
+            </div>
+            <div className="col-4 col-lg-6">
+              <h2 className="ff_inter fw-bold fs_9xl text_gradient mb-0 text-center text-lg-start mt-2 mt-lg-0">
+                Learn English{" "}
+              </h2>
+              <p className="ff_inter fw-semibold fs_6xl color_dark_blue mb-0 text-center text-lg-start">
+                Spoken Courses & Language Courses
+              </p>
+              <p className="ff_inter fs_md color_dark_blue mb-0 text-center text-lg-start">
+                By Wonder Girl Janvavi Panwar
+              </p>
+            </div>
+            <div className="col-3 col-lg-2 text-center text-lg-start">
+              <Link to="/register">
+                <button className="fs_xl text-white ff_inter bg_gradient border-0 px-2 py-1 px-sm-4 py-sm-2 ">
+                  Register
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className="col-lg-6">
-            <h2 className="ff_inter fw-bold fs_9xl text_gradient mb-0 text-center text-lg-start mt-2 mt-lg-0">
-              Learn English{" "}
-            </h2>
-            <p className="ff_inter fw-semibold fs_6xl color_dark_blue mb-0 text-center text-lg-start">
-              Spoken Courses & Language Courses
-            </p>
-            <p className="ff_inter fs_md color_dark_blue mb-0 text-center text-lg-start">
-              By Wonder Girl Janvavi Panwar
-            </p>
-          </div>
-          <div className="col-lg-2 text-center text-lg-start">
-            <Link to="/register">
-              <button className="fs-6 text-white ff_inter bg_gradient border-0 px-4 py-2 mt-3 mt-lg-0">
-                Register
-              </button>
-            </Link>
+        </div>
+        <div className="px-sm-3 px-1">
+          <div className="d-flex align-items-center learn py-lg-0">
+            <div className="col-5 col-lg-4 position-relative">
+              <img className="w-100" src={BgEllipse} alt="BgEllipse" />
+              <img
+                className="position-absolute janhavi_img start-50 translate-middle-x "
+                src={JanhaviImg}
+                alt="JanhaviImg"
+              />
+            </div>
+            <div className="col-4 col-lg-6">
+              <h2 className="ff_inter fw-bold fs_9xl text_gradient mb-0 text-center text-lg-start mt-2 mt-lg-0">
+                Learn English{" "}
+              </h2>
+              <p className="ff_inter fw-semibold fs_6xl color_dark_blue mb-0 text-center text-lg-start">
+                Spoken Courses & Language Courses
+              </p>
+              <p className="ff_inter fs_md color_dark_blue mb-0 text-center text-lg-start">
+                By Wonder Girl Janvavi Panwar
+              </p>
+            </div>
+            <div className="col-3 col-lg-2 text-center text-lg-start">
+              <Link to="/register">
+                <button className="fs_xl text-white ff_inter bg_gradient border-0 px-2 py-1 px-sm-4 py-sm-2 ">
+                  Register
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </Slider>
