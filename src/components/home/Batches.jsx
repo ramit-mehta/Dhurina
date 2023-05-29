304fcdd24493c4738587abfdc86f07c473e9e45a
Ramit Mehta <mail.ramit2499@gmail.com>
2023-05-29 13:17:40 +0530

    integrating api

--- src/components/home/Batches.jsx
+++ src/components/home/Batches.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useState, useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { Link } from "react-router-dom";
